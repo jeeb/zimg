de756bedb71a9609b024609e93d2479471aa2c24
Jan Ekström <jeebjp@gmail.com>
2017-03-04 02:12:20 +0200

    debian: remove example files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(zimg):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="zimg" command="/usr/bin/zimg"
--- debian/zimg.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the zimg package
-#
-0 4	* * *	root	[ -x /usr/bin/zimg_maintenance ] && /usr/bin/zimg_maintenance
